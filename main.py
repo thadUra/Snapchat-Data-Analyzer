fe83f13de6cd76a22b47ad084538d5083213e784
Thaddeus Dziura <49488898+thaddyD@users.noreply.github.com>
2022-06-29 16:30:09 -0700

    Update main.py
    Implemented a few functions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import json
+from matplotlib.cbook import simple_linear_interpolation
 import pandas
 import matplotlib.pyplot as plt
 
@@ -20,8 +21,8 @@ def extract( file, category, objects ):
     return dFrame
 
 '''
-Function prints out total number of friends, five most recently added friends, and five oldest friends
-TODO: if time, add blocked and removed friend categories
+Function prints out total number of friends, five most recently added friends, and five oldest friends 
+within the given data timeframe
 '''
 def friendStats():
     # friend.json extraction
@@ -34,11 +35,14 @@ def friendStats():
     dFrame["Date Added"] = pandas.to_datetime(dFrame["Date Added"], format="%Y-%m-%d %H:%M:%S %Z")
     dFrame.sort_values(by=["Date Added"], inplace=True, ascending=False)
     dFrame["Date Added"] = dFrame["Date Added"].dt.strftime("%m/%d/%Y")
+    startDate = dFrame.iloc[-1]['Date Added']
+    endDate = dFrame.iloc[0]['Date Added']
 
     # Printing Results
     print('\033[1m', "Total Number of Friends: ", '\033[0m', dFrame.shape[0], "\n")
     print('\033[1m', "Five Most Recent Friends:\n",'\033[0m', dFrame.head())
     print('\033[1m', "\nFive Oldest Friends:\n",'\033[0m', dFrame.tail(), "\n")
+    print('\033[1m', "Date Range for Data Values: ", '\033[0m', startDate, " to ", endDate, "\n")
 
 
 '''
@@ -71,8 +75,8 @@ def snapStats():
     # snap_history.json extracation
     cols = [["From", "Media Type", "Created"], ["To", "Media Type", "Created"]]
     categories = ["Received Snap History", "Sent Snap History"]
-    dFrames = [ extract("chat_history.json", categories[0], cols[0]), 
-                extract("chat_history.json", categories[1], cols[1])]
+    dFrames = [ extract("snap_history.json", categories[0], cols[0]), 
+                extract("snap_history.json", categories[1], cols[1])]
 
     # Parsing dataframes for print
     # TODO
@@ -82,27 +86,44 @@ def snapStats():
         print( i.head())
 
 '''
-Function prints out total stories posted, most story views, most story replies, most stories viewed 
-on other users
+Function prints out most / least stories viewed and total number of stories viewed within the given data timeframe
 '''
 def storyStats():
-    cols = [["Story Date", "Story Views", "Story Replies"], ["View"]]
-    categories = ["Your Story Views", "Friend and Public Story Views"]
-    dFrames = [ extract("chat_history.json", categories[0], cols[0]), 
-                extract("chat_history.json", categories[1], cols[1])]
+    # story_history.json extraction
+    cols = ["View", "Media Type", "View Date"]
+    categories = ["Friend and Public Story Views"]
+    dFrame = extract("story_history.json", categories[0], cols)
 
     # Parsing dataframes for print
-    # TODO
+    dFrame["View Date"] = pandas.to_datetime(dFrame["View Date"], format="%Y-%m-%d %H:%M:%S %Z")
+    dFrame.sort_values(by=["View Date"], inplace=True, ascending=False)
+    dFrame["View Date"] = dFrame["View Date"].dt.strftime("%m/%d/%Y")
+    startDate = dFrame.iloc[-1]['View Date']
+    endDate = dFrame.iloc[0]['View Date']
+
+    storyViews = {}
+    totalStories = 0
+    for index, row in dFrame.iterrows():
+        if row["Media Type"] == "STORY" or row["Media Type"] == "VIDEO":
+            key = row["View"]
+            storyViews[key] = storyViews.get(key, 0) + 1
+            totalStories += 1
+    dFrame = pandas.DataFrame(list(storyViews.items()), columns=['Username', 'Views'])
+    dFrame.sort_values(by=["Views"], inplace=True, ascending=False)
 
     # Printing Results
-    for i in dFrames:
-        print( i.head())
+    print('\033[1m', "Total Number of User Stories Viewed: ", '\033[0m', totalStories, "\n")
+    print('\033[1m', "Five Most Viewed User Stories:\n",'\033[0m', dFrame.head())
+    print('\033[1m', "\nFive Least Viewed User Stories:\n",'\033[0m', dFrame.tail(), "\n")
+    print('\033[1m', "Date Range for Data Values: ", '\033[0m', startDate, " to ", endDate, "\n")
+
 
 '''
 Function prints out breakdown of time spent on app and creates pie chart of values
 '''
 def timeStats():
-    # user_profile.json extraction
+    # user_profile.json extraction 
+    # cannot use extract function with different file structure
     jsonFile = open("user_profile.json")
     jsonText = json.load(jsonFile)
     jsonString = jsonText["Breakdown of Time Spent on App"]
@@ -116,12 +137,12 @@ def timeStats():
         temp = i.split(": ", 1)
         categories.append(temp[0])
         percentSpent.append(float(temp[1].strip('%')))
-    print("\n")
     
     # Displaying pie chart of time breakdown
     dFrame = pandas.DataFrame({'percentages': percentSpent}, index=categories)
     plot = dFrame.plot.pie(y='percentages', figsize=(5,5))
     plt.show()
+    print("\n")
 
 
 # Main loop for analyzer
